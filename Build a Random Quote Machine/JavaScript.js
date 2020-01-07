aa9e79067d4e5935d50b23ed19aa5dfe159cc706
GosuAlex <super.atx@gmail.com>
2020-01-08 00:44:14 +0100

    more quote machine

--- Build a Random Quote Machine/JavaScript.js
+++ Build a Random Quote Machine/JavaScript.js
@@ -1,15 +1,27 @@
+const { TransitionGroup, CSSTransition } = ReactTransitionGroup;
+
 const QuoteBox = (props) => {
   return (
-    <div className="boxContainer">
-      <div>
-        <h6 className="quote">{props.currentQuote.text}</h6>
-      </div>
-      <p className="author">{props.currentQuote.author}</p>
-      <div className="footer">
-        <a href={`https://twitter.com/intent/tweet?hashtags=InspirationalQuotes&text=${props.currentQuote.text}`}><i className="fab fa-twitter-square tweetIcon"></i></a>
-        <button className="btn" onClick={props.pickRandomQuote} >New Quote</button>
+        <CSSTransition
+      in={true}
+      appear={true}
+      timeout={1000}
+      classNames="previousContainer"
+      id="quote-box"
+    >
+    <div className="boxContainer" id="quote-box">
+      <div className="boxCenterColor!IGNORE">
+        <div>
+          <h6 className="quote" id="text">{props.currentQuote.text}</h6>
+        </div>
+        <p className="author" id="author">{props.currentQuote.author}</p>
+        <div className="footer">
+          <a href={`https://twitter.com/intent/tweet?hashtags=InspirationalQuotes&text=${props.currentQuote.text}`}><i className="fab fa-twitter-square tweetIcon" id="tweet-quote"></i></a>
+          <button className="btn" id="new-quote" onClick={props.pickRandomQuote} >New Quote</button>
+        </div>
       </div>
     </div>
+      </CSSTransition>
   );
 };
 
@@ -21,6 +33,7 @@ const PreviousQuotesBoxes = (props) => {
           <h6 className="quote">{quote.text}</h6>
         </div>
         <p className="author">{quote.author}</p>
+        <a href={`https://twitter.com/intent/tweet?hashtags=InspirationalQuotes&text=${quote.text}`}><i className="fab fa-twitter-square tweetIcon"></i></a>
       </div>
     );
   });
@@ -72,12 +85,12 @@ class App extends React.Component {
   
   render() {
     return (
-      <main id="test">
+      <main>
         <h1>Quote Machine</h1>
         <QuoteBox 
           currentQuote={this.state.currentQuote}
           pickRandomQuote={this.pickRandomQuote} />
-        {this.state.showPreviousHeader ? <h1>Previous Quotes:</h1> : null}
+        {this.state.showPreviousHeader ? <h1>Previous Quotes</h1> : null}
         <PreviousQuotesBoxes 
           previousQuotesArray={this.state.previousQuotesArray} />
       </main> 
