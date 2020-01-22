03de89b0dbc30ecd397e7baa00bfe97aa81ff97a
GosuAlex <super.atx@gmail.com>
2020-01-22 08:07:06 +0100

    virtually done 1.1

--- Build a JavaScript Calculator/JavaScript.js
+++ Build a JavaScript Calculator/JavaScript.js
@@ -1,6 +1,7 @@
-// FCCCalculatorUI was only used to satisfy FreeCodeCamp's user stories requirements
-// 0.X-0. doesnt work.
-const FCCCalculatorUI = (props) => {
+// FreeCodeCampCalculatorUI component was only used to satisfy FreeCodeCamp's user stories requirements
+// todo: make the top showing auto answer after operator or CE
+// better screen.. size, font, color, numbers showing
+const FreeCodeCampCalculatorUI = (props) => {
   let calcButtons = [1,2,3,4,5,6,7,8,9,0,"+","-","X","÷","=",".","C","CE","<"];
   const FCCRequirements = ["one","two","three","four","five","six","seven","eight","nine","zero","add","subtract","multiply","divide","equals","decimal","clear","CE","<"];
   let UIButtons = calcButtons.map((btn, idx) => {
@@ -40,8 +41,8 @@ const CalculatorScreen = (props) => {
     <div className={`${props.calcClass}Screen`}>
       {/*<p id="">{props.history ? props.history : "" }</p>
       <p id="display">{props.screenValue[0] != null ? props.screenValue : "0" }</p>*/}
-      <p id="">{props.history ? props.history.toString().length > 16 ? props.history.join("").substring(0,16).concat("..") : props.history : "" }</p>
-      <p id="display">{props.screenValue[0] != null ? props.screenValue.toString().length > 16 ? props.screenValue.join("").substring(0,16).concat("..") : props.screenValue  : "0" }</p>
+      <p id="topScreen">{props.history ? props.history.join("").length > 16 ? props.history.join("").substring(0,16).concat("..") : props.history : "" }</p>
+      <p id="display">{props.screenValue[0] != null ? props.screenValue.join("").length > 16 ? props.screenValue.join("").substring(props.screenValue.join("").length-16,props.screenValue.join("").length) : props.screenValue  : "0" }</p>
     </div>
   );
 };
@@ -97,19 +98,18 @@ class App extends React.Component {
   
   addDot = (dot) => {
     const newState = this.state.screenValue;
-    if(!newState[newState.length-1] || Number.isNaN(Number(newState[newState.length-1]))) {
+    if((newState[newState.length-2] == "X" || newState[newState.length-2] == "÷") && newState[newState.length-1] == "-" ) {
+      newState[newState.length-1] += "0" + dot;
+    } else if (!newState[newState.length-1] || Number.isNaN(Number(newState[newState.length-1]))) {
       newState.push("0.");
-      this.setState({
-        screenValue: newState 
-      });
     } else if((/[.]/).test(this.state.screenValue[this.state.screenValue.length-1]) || Number.isNaN(Number(this.state.screenValue[this.state.screenValue.length-1]))) {
       return;
     } else {
       newState[newState.length-1] += dot;
-      this.setState({
-        screenValue: newState 
-      });
     }
+    this.setState({
+      screenValue: newState 
+    });
   }
   
   addNumber = (number) => {
@@ -120,7 +120,7 @@ class App extends React.Component {
     } else if (!Number.isNaN(Number(newState[newState.length-1]))) {
       /*If last item is number, just add current digit to it*/
       newState[newState.length-1] += number;
-    } else if ((newState[newState.length-2] == "X" || newState[newState.length-2] == "÷") || newState[0] == "-" ) {
+    } else if ((newState[newState.length-2] == "X" || newState[newState.length-2] == "÷" && newState[newState.length-1] == "-") || newState[0] == "-" ) {
       /*If there are X OR ÷ before - OR want first number to be negative, move - into current number so it becomes: number (x||÷) -number and not 2 operators after each other.*/
       newState[newState.length-1] = "-" + number;
     } else {
@@ -194,7 +194,6 @@ class App extends React.Component {
   multiplicationAndDivision = (arr) => {  
     while(arr.includes("X")) {
       let idx = arr.indexOf("X")
-      console.log(arr[idx - 1] + " " + arr[idx] + " " + arr[idx + 1] + "her");
       let calcIteration = Number(arr[idx - 1]) * Number(arr[idx + 1]);
       arr[idx - 1] = Number(calcIteration.toFixed(4)).toString();
       arr.splice(idx, 2);
@@ -238,14 +237,10 @@ class App extends React.Component {
     return (
       <main>
         <h1>JavaScript Calculator</h1>
-
         <CalculatorScreen
           calcClass={this.state.calcClass}
           screenValue={this.state.screenValue}
           history={this.state.history} />
-        {/*<FCCCalculatorUI
-          calcClass={this.state.calcClass}
-          getButtonPressed={this.getButtonPressed} />*/}
         <CalculatorUI
           calcClass={this.state.calcClass}
           getButtonPressed={this.getButtonPressed} />
